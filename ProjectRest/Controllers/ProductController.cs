32baab0ef6c2d8b86a52d599a105432b800d2cde
Medo Mohsen <41289533+Redcode88@users.noreply.github.com>
2022-05-24 14:41:56 +0200

    New Pr-Actions

--- ProjectRest/Controllers/ProductController.cs
+++ ProjectRest/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProjectRest.DB;
+using ProjectRest.Entity;
 using ProjectRest.Repositories;
 
 namespace ProjectRest.Controllers
@@ -29,7 +30,7 @@ namespace ProjectRest.Controllers
             {
                 return BadRequest("There's No data on Product Table");
             }
-            return Ok(data);
+            return new JsonResult(data);
 
         }
 
@@ -43,11 +44,37 @@ namespace ProjectRest.Controllers
             {
                 return BadRequest("There's No data on Product Table");
             }
-            return Ok(data);
-
-            
+            return new JsonResult(data);
         }
 
+
+        [Route("AddProduct")]
+        [HttpPost]
+        public IActionResult AddProduct(Product model)
+        {
+            ProductRepository pr = new ProductRepository(_context);
+            if (ModelState.IsValid)
+            {
+                pr.Add(model);
+                return Ok(new string("Product Add Done"));
+            }
+            return BadRequest("There's Error with data you have enterd");
+
+        }
+
+        [Route("DeleteProduct")]
+        [HttpPost]
+        public IActionResult DeleteProduct([FromBody] int Id)
+        {
+            var ProductId = _context.Products.Where(p => p.ProductId == Id).FirstOrDefault();
+            ProductRepository pr = new ProductRepository(_context);
+            if (Id == 0)
+            {
+                return BadRequest("you should Select Product To delete");
+            }
+            pr.Remove(ProductId);
+            return Ok(new string("Product Delete Done"));
+        }
 
     }
 }
